b73b77d665b1cc9545fa28400905659f527874dd
Rytuo <popov.aleksandr.v01@gmail.com>
2021-05-19 21:55:10 +0300

    skeleton added

--- lab3/lab3.h
+++ lab3/lab3.h
@@ -1,9 +1,27 @@
 #ifndef METOPT_LAB3_H
 #define METOPT_LAB3_H
 
-class lab3 {
+// здесь должны быть инклуды, дефайны и вспомогательные структуры для методов
+#include <utility>
+#include <vector>
+
+class profile_matrix {
+private:
+    std::vector<double> d;
+    std::vector<int> ia;
+    std::vector<double> al;
+    std::vector<double> au;
+public:
 
 };
 
+class default_matrix {
+private:
+    std::vector<std::vector<double>> m;
+public:
+    explicit default_matrix(std::vector<std::vector<double>> m) {
+        this->m = std::move(m);
+    }
+};
 
 #endif //METOPT_LAB3_H
