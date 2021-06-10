0bc3fbfdc0b45fbdf19b318c0ac5d95cccd7b571
Rytuo <popov.aleksandr.v01@gmail.com>
2021-06-11 02:39:10 +0300

    lab4 quazinewton test added

--- lab4/tests.h
+++ lab4/tests.h
@@ -90,7 +90,32 @@ void test_quazinewtons(extended_function &f, const vector_ &x0) {
 }
 
 void test_quazinewtons_fixed() {
+    auto func1 = [](const vector_ &x) {
+        return 100 * std::pow(x[0], 4) - 200 * x[0] * x[0] * x[1] + 100 * x[1] * x[1] + x[0] * x[0] + 1 - 2 * x[0];
+    };
+    auto grad1 = [](const vector_ &x) {
+        return vector_ {400 * std::pow(x[0], 3) - 400 * x[0] * x[1] + 2 * x[0] - 2, -200 * x[0] * x[0] + 200 * x[1]};
+    };
+    auto gess1 = [](const vector_ &x) {
+        return matrix_ {{1200 * x[0] * x[0] - 400 * x[1] + 2, -400 * x[0]},
+                        {-400 * x[0], 200}};
+    };
+    extended_function f1 = extended_function(func1, grad1, gess1);
+    test_quazinewtons(f1, {0, 0});
 
+    auto func2 = [](const vector_ &x) {
+        return std::pow(x[0] * x[0] + x[1] - 11, 2) + std::pow(x[0] + x[1] * x[1] - 7, 2);
+    };
+    auto grad2 = [](const vector_ &x) {
+        return vector_ {4 * std::pow(x[0], 3) + 4 * x[0] * x[1] + 2 * x[1] * x[1] - 42 * x[0] - 14,
+                        4 * std::pow(x[1], 3) + 4 * x[0] * x[1] + 2 * x[0] * x[0] - 26 * x[1] - 22};
+    };
+    auto gess2 = [](const vector_ &x) {
+        return matrix_ {{12 * x[0] * x[0] + 4 * x[1] - 42, 4 * x[1] + 4 * x[0]},
+                        {4 * x[0] + 4 * x[1], 12 * x[1] * x[1] + 4 * x[0] - 26}};
+    };
+    extended_function f2 = extended_function(func2, grad2, gess2);
+    test_quazinewtons(f2, {0, 0});
 }
 
 #endif //METOPT_TESTS_H
